23b6e26f481c6a30b561059a0e7aaf40eda5b11d
Kevin <kevin_zino@yahoo.com>
2024-05-10 01:48:38 +0300

    ci(BH-20): update release config

--- release.config.ts
+++ release.config.ts
@@ -4,7 +4,7 @@ const config = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     ["@semantic-release/git", {
-      "assets": ["package.json", "CHANGELOG.md"],
+      "assets": ["dist/*.js", "dist/*.js.map"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
     '@semantic-release/github'
